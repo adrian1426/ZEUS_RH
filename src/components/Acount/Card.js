7c9f829e545ea7b73f3a0064c9e4523c4d707254
Adrian1426 <adrianhh1426@gmail.com>
2020-05-24 17:37:34 -0500

    cambios develop

--- src/components/Acount/Card.js
+++ src/components/Acount/Card.js
@@ -1,21 +1,15 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import Cards from 'react-credit-cards';
 import 'react-credit-cards/es/styles-compiled.css';
 
-class Card extends Component{
-    render(){
-        return(
-            <div id="body-container">
-                <Cards
-                number="1223"
-                name="adr"
-                expiry="ass"
-                cvc="asss"
-                focused=""
-                />
-            </div>
-        );
-    }
+class Card extends Component {
+  render() {
+    return (
+      <div id="body-container">
+        <Cards number="1223" name="adrian" expiry="ass" cvc="asss" focused="" />
+      </div>
+    );
+  }
 }
 
 export default Card;
